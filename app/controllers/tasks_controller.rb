6433fd599bfa59e0018b45d3425197327a0f9c60
Masahiro.Nakamura <nakamuramasakayo@me.com>
2018-03-03 09:27:34 +0000

    kadai login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,8 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
+  before_action :require_user_logged_in
+  before_action :set_task, only: [:show, :edit, :update]
+  before_action :correct_user, only: [:destroy]
+
   def index
     @tasks = Task.all.page(params[:page]).per(13)
   end
@@ -9,18 +11,22 @@ class TasksController < ApplicationController
   end
   
   def new
-    @task = Task.new
+    # Task.new ... 「タスクを新規作成する」
+    # current_user.tasks.new ... 「"吉田くんの" タスクを新規作成する」
+    # @task = Task.new
+    @task = current_user.tasks.new
   end
   
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.new(task_params)
     
     if @task.save
       flash[:success] = 'Taskが正常に作成されました'
-      redirect_to @task
+      redirect_to root_url
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = 'Taskが作成されませんでした'
-      render :new
+      render 'toppages/index'
     end
   end
 
@@ -40,9 +46,8 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    
     flash[:success] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to(root_path)
   end
   
   private
@@ -53,5 +58,12 @@ class TasksController < ApplicationController
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
